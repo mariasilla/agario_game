4f8f4ed1631c208e2da10e04179b16c6ea112134
Silla <maria.sylla@gmail.com>
2018-08-17 15:06:05 +0300

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,61 @@
-
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const postcssUrl = require('postcss-url');
+const cssnano = require('cssnano');
+const customProperties = require('postcss-custom-properties');
+
 
 module.exports = {
-  entry: './src/index.js',
-  mode: 'development',
-  output: {
-    "filename": "[name].bundle.js",
-    path: path.join(process.cwd(), "dist")
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  }
+entry: {
+main: './src/index.js',
+styles: './src/style.css'
+},
+mode: 'development',
+output: {
+"filename": "[name].bundle.js",
+"chunkFilename": "[id].chunk.js",
+path: path.join(process.cwd(), "dist")
+},
+module: {
+rules: [
+{
+"test": /\.css$/,
+"loaders": ExtractTextPlugin.extract({
+"use": [
+{
+"loader": "css-loader",
+"options": {
+"sourceMap": true
+}
+},
+{
+"loader": "postcss-loader",
+"options": {
+"sourceMap": true
+}
+}
+],
+"publicPath": ""
+})
+},
+{
+"test": /\.js$/,
+"exclude": /node_modules/,
+"loader": "babel-loader"
+}
+]
+},
+plugins: [
+new CleanWebpackPlugin(['dist']),
+new ExtractTextPlugin({
+"filename": "[name].bundle.css"
+}),
+new HtmlWebpackPlugin({
+filename: 'index.html'
+})
+]
 };
-
 
