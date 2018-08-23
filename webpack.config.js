f5fb6b2d61bddf6e551951139f0dcab7d3470923
Silla <maria.sylla@gmail.com>
2018-08-23 17:41:39 +0300

    sass-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,8 @@
 const path = require('path');
-// const SassPlugin = require('sass-webpack-plugin');
 const HtmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
-    plugins: [
-        new SassPlugin('./src/style.scss', process.env.NODE_ENV)
-    ],
     mode: 'development',
     output: {
         filename: 'build.js',
