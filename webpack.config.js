7c5476da8842d47a4e31cff260ee6cb014d28417
Silla <maria.sylla@gmail.com>
2018-08-23 17:10:29 +0300

    sass-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,24 @@
 const path = require('path');
+const SassPlugin = require('sass-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
-entry: './src/index.js',
-mode: 'development',
-output: {
-filename: 'build.js',
-path: path.resolve(__dirname, 'src')
-},
-module: {
-rules: [
-{
-test: /\.css$/,
-use:['style-loader','css-loader']
-}
-]
-}
+    entry: './src/index.js',
+    plugins: [
+        new SassPlugin('./src/style.scss', process.env.NODE_ENV)
+    ],
+    mode: 'development',
+    output: {
+        filename: 'build.js',
+        path: path.resolve(__dirname, 'src')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
 };
 
